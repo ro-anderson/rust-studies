608d3e5f816a23c544e787214ad21287f42a2b2f
ro-anderson <didier.rda@gmail.com>
2024-12-31 15:04:09 -0300

    chore: first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rust-studies
+
+based on:
+https://doc.rust-lang.org/rust-by-example/index.html
---
+++ code/hello_world_macro.rs
@@ -0,0 +1,22 @@
+// This is a simple macro named `say_hello`.
+macro_rules! say_hello {
+    // `()` indicates that the macro takes no argument.
+    () => {
+        // The macro will expand into the contents of this block.
+        println!("Hello macro!")
+    };
+}
+
+fn main() {
+    // This call will expand into `println!("Hello!")`
+    say_hello!()
+}
+
+
+//So why are macros useful?
+// https://doc.rust-lang.org/rust-by-example/macros.html
+//Don't repeat yourself. There are many cases where you may need similar functionality in multiple places but with different types. Often, writing a macro is a useful way to avoid repeating code. (More on this later)
+//
+//Domain-specific languages. Macros allow you to define special syntax for a specific purpose. (More on this later)
+//
+//Variadic interfaces. Sometimes you want to define an interface that takes a variable number of arguments. An example is println! which could take any number of arguments, depending on the format string. (More on this later)
